247320e98b5c734106402af2d2cffc7780c09080
movas <nikos.gogalis.04@gmail.com>
2026-03-31 03:43:02 +0300

    Add nuxt-security module to enhance application security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 
 export default defineNuxtConfig({
+    modules: ["nuxt-security"],
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
 
